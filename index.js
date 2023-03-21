adb8c1a6cbd53c591afdf57e023e16ab5f1abb3d
calixteStephane1977 <126086360+calixteStephane1977@users.noreply.github.com>
2023-03-21 17:13:51 +0100

    21/03 17:00
    uhoiyfi

---
+++ index.js
@@ -0,0 +1,38 @@
+// =================EXERCICE 1===============
+
+const age =25 ;
+console.log(age);
+// =================EXERCICE 2===============
+const nom ="Jean";
+console.log(nom);
+// =================EXERCICE 3===============
+const somme = 3+5 ;
+console.log(somme);
+// =================EXERCICE 4===============
+let chaine="Ma variable"
+chaine=chaine.toUpperCase();
+console.log(chaine);
+// =================EXERCICE 5===============
+const tableau =[1,3,4,5].length;
+// =================EXERCICE 6===============
+const objet ={
+    nom:"calixte",
+    age:"stephane"
+};
+console.log(objet.nom)
+// =================EXERCICE 7===============
+
+const nombre =2.5568
+let nombre1 =nombre.toFixed(2)
+console.log(nombre1);
+// =================EXERCICE 8===============
+const d=new Date();
+let month = d.getMonth();
+
+console.log(month);
+// =================EXERCICE 9===============
+
+let tableau2 = ["martial","roger","tristan"].join("");
+// ===================================
+console.log(tableau2);
+
